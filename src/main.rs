bf47b315b391be18589efbd2ba27800235a13baf
saiumesh <saiumesh.dhanewar@apty.io>
2022-06-06 13:56:16 +0530

    printing error

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ mod types;
 use env_logger::Env;
 use errors::ESDumpResult;
 use es_dump::ESDump;
-use log::info;
+use log::{info, error};
 
 #[tokio::main]
 async fn main() -> ESDumpResult<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     info!("Service Started...");
-    ESDump::new().dump().await?;
+    let result = ESDump::new().dump().await;
+    if result.is_err() {
+        error!("{}", result.err().unwrap());
+    }
     Ok(())
 }
